282c1c9ee50acaa552d7033f3bfde8d0e5d3f671
Chiza Barber <chiza.alba@gmail.com>
2018-10-02 19:43:35 -0500

    after standard

--- exercises/01-predicate-functions.js
+++ exercises/01-predicate-functions.js
@@ -22,13 +22,13 @@
 // isVowel({e: 'Elephant'}) --> false
 
 function isVowel (char) {
-    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-        return true
-    } if (char === "A" || char === "E" || char === "I" || char === "O" || char === "U") {
-        return true
-    } else {
-        return false 
-    }
+  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+    return true
+  } if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+    return true
+  } else {
+    return false
+  }
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -46,19 +46,19 @@ function isVowel (char) {
 // isOdd(3.14) --> false
 
 function isEven (num) {
-    if (num % 2 === 0) {
-        return true
-    } else {
-        return false
-    }
+  if (num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function isOdd (num) {
-    if (num % 2 === 1 || num % 2 === -1) {
-        return true
-    } else {
-        return false
-    }
+  if (num % 2 === 1 || num % 2 === -1) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -76,31 +76,65 @@ function isOdd (num) {
 // isCapitalCity('Strawberry', 'Mango') --> false
 
 function isCapitalCity (state, city) {
-    const pairs = {"Alabama":"Montgomery","Alaska":"Juneau","Arizona":"Phoenix",
-                    "Arkansas":"Little Rock","California":"Sacramento","Colorado":"Denver",
-                    "Connecticut":"Hartford","Delaware":"Dover","Florida":"Tallahassee",
-                    "Georgia":"Atlanta","Hawaii":"Honolulu","Idaho":"Boise","Illinois":"Springfield",
-                    "Indiana":"Indianapolis","Iowa":"Des Moines","Kansas":"Topeka","Kentucky":"Frankfort",
-                    "Louisiana":"Baton Rouge","Maine":"Augusta","Maryland":"Annapolis",
-                    "Massachusetts":"Boston","Michigan":"Lansing","Minnesota":"Saint Paul",
-                    "Mississippi":"Jackson","Missouri":"Jefferson City","Montana":"Helana",
-                    "Nebraska":"Lincoln","Nevada":"Carson City","New Hampshire":"Concord",
-                    "New Jersey":"Trenton","New Mexico":"Santa Fe","New York":"Albany",
-                    "North Carolina":"Raleigh","North Dakota":"Bismarck","Ohio":"Columbus",
-                    "Oklahoma":"Oklahoma City","Oregon":"Salem","Pennsylvania":"Harrisburg",
-                    "Rhode Island":"Providence","South Carolina":"Columbia","South Dakota":"Pierre",
-                    "Tennessee":"Nashville","Texas":"Austin","Utah":"Salt Lake City","Vermont":"Montpelier",
-                    "Virginia":"Richmond","Washington":"Olympia","West Virginia":"Charleston",
-                    "Wisconsin":"Madison","Wyoming":"Cheyenne"};
-    var states = Object.keys(pairs);
-    var cities = Object.values(pairs);
-    if ( states.includes(state) === true && cities.includes(city) === true ) {
-      if ( states.indexOf(state) != cities.indexOf(city) ) {
-        return false;
-      } else {
-        return true;
-      }
+  const pairs = { 'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Moines',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'Saint Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helana',
+    'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakota': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne' }
+  var states = Object.keys(pairs)
+  var cities = Object.values(pairs)
+  if (states.includes(state) === true && cities.includes(city) === true) {
+    if (states.indexOf(state) != cities.indexOf(city)) {
+      return false
     } else {
-      return false;
+      return true
     }
+  } else {
+    return false
   }
+}
